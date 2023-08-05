efd5af6819b791d5d5ecd8c82646313f5fa62ae0
Karan Raj Singh <96483154+Karan-Raj-Singh@users.noreply.github.com>
2023-08-05 13:00:33 +0100

    Add files via upload

---
+++ Introduction to Python World.py
@@ -0,0 +1,39 @@
+# data types
+# String,Float,Integer and a Boolean
+# variable
+# something that has been assigned a particular data type
+x="karan"
+print(x)
+y=-1
+print(y)
+z=1.5
+print(z)
+v=True
+print(v)
+
+#Operators
+# +,-,*,**,/,//,%
+x=2
+y=3
+print(x+y)
+print(x-y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x*y)
+
+# at once we can assign all the values
+x,y,z=1,2,3,
+print(x)
+print(y)
+print(z)
+# check the datatype of the code
+# using the data type function
+
+a=1
+b= True
+c=1.5
+
+print(type(a))
+print(type(b))
+print(type(c))
