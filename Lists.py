65ba21e2b3ee006dd84b8c788a6e1b46cda23fc8
Karan Raj Singh <96483154+Karan-Raj-Singh@users.noreply.github.com>
2023-08-07 02:38:24 +0100

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,22 @@
+list_1=["karan",1, 2.4, False]
+print(list_1)
+
+list_2=[]
+# appends to add elements from the end of the list
+list_2.append(1)
+list_2.append("F")
+print(list_2)
+
+# Numerical list
+# append, insert, sum, lemgth, minimum and maximum, sort as well as the pop function, count
+list_3=[1,2,3,4,5,6,7,8,9,0]
+print(list_3.append(111))
+print(list_3.sort(reverse=True))
+print(list_3.insert(1,22))
+print(sum(list_3))
+print(len(list_3))
+print(min(list_3))
+print(max(list_3))
+print(list_3.count(1))
+print(list_3.pop(0))
+print(list_3)
